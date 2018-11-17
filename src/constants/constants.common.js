9b5d86a4ea09b6bbd6c07c45be76f2e769ed2109
Souvik Chakraborty <souvik.c5@tcs.com>
2018-11-17 18:29:00 +0530

    created firebase authentication environment and middleware implementation

--- src/constants/constants.common.js
+++ src/constants/constants.common.js
@@ -6,31 +6,45 @@
  * constant.[env].js variables
  */
 const shape = {
-    logs: undefined,
-    corsOptions: {
-        origin: undefined,
-        credentials: undefined,
-    },
+  logs: undefined,
+  corsOptions: {
+    origin: undefined,
+    credentials: undefined,
+  },
 };
 
 /**
  * Common constants across all the environments (dev, staging, prod)
  */
 module.exports = {
-    env: process.env.NODE_ENV,
-    port: process.env.PORT,
+  env: process.env.NODE_ENV,
+  port: process.env.PORT,
 
-    database: {
-        uri: process.env.DB_URI,
-        database: process.env.DB_DB_NAME,
-        collections: {
-            user: process.env.DB_USER_COLLECTION,
-        },
+  database: {
+    uri: process.env.DB_URI,
+    database: process.env.DB_DB_NAME,
+    collections: {
+      user: process.env.DB_USER_COLLECTION,
     },
-
-    dump: {
-        secret: process.env.DUMP_SECRET,
+  },
+  firebase: {
+    firebaseServiceAccount: {
+      type: process.env.TYPE,
+      project_id: process.env.PROJECT_ID,
+      private_key_id: process.env.PRIVATE_KEY_ID,
+      private_key: process.env.PRIVATE_KEY,
+      client_email: process.env.CLIENT_EMAIL,
+      client_id: process.env.CLIENT_ID,
+      auth_uri: process.env.AUTH_URI,
+      token_uri: process.env.TOKEN_URI,
+      auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+      client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
     },
+    databaseURL: process.env.DATABASE_URL,
+  },
+  dump: {
+    secret: process.env.DUMP_SECRET,
+  },
 
-    ...shape,
+  ...shape,
 };
