c2f287f1e96af86803df9033b4e2a851cf9d6317
Souvik Chakraborty <souvik.c5@tcs.com>
2018-11-17 19:19:40 +0530

    authenticated middleware functioning

--- src/api/middlewares/authenticated.js
+++ src/api/middlewares/authenticated.js
@@ -23,6 +23,7 @@ module.exports = (req, res, next) => {
     })
     .catch((error) => {
       logger.error(error);
+      return res.status(httpStatus.UNAUTHORIZED).json(responses.unAuthorized());
     });
   /**
    * If the incoming request contains proper cookies,
@@ -36,8 +37,4 @@ module.exports = (req, res, next) => {
    * you might want to check req.session.id ( in case of cookies )
    * or req.headers['x-access-token'] and then validate the request
    */
-  if (req.user) {
-    return next();
-  }
-  return res.status(httpStatus.UNAUTHORIZED).json(responses.unAuthorized());
 };
