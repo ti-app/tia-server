fe0da9136fe943228cbc5ac9a3ffbc3d2f36df5f
Anand Undavia <abundavia@gmail.com>
2018-12-16 20:11:23 +0530

    Added replace() to avoid firebase private keys issues

--- src/constants/constants.common.js
+++ src/constants/constants.common.js
@@ -37,7 +37,7 @@ module.exports = {
       type: process.env.TYPE,
       project_id: process.env.PROJECT_ID,
       private_key_id: process.env.PRIVATE_KEY_ID,
-      private_key: process.env.PRIVATE_KEY,
+      private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
       client_email: process.env.CLIENT_EMAIL,
       client_id: process.env.CLIENT_ID,
       auth_uri: process.env.AUTH_URI,
