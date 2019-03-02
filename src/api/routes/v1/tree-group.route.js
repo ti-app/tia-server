1a3fce5808de28eb298b5324bfdbb018ada5e8a4
Souvik Chakraborty <souvik.c5@tcs.com>
2019-03-03 00:52:46 +0530

    added google cloud storage for image upload

--- src/api/routes/v1/tree-group.route.js
+++ src/api/routes/v1/tree-group.route.js
@@ -4,8 +4,17 @@ const validate = require('express-validation');
 const controller = require('../../controllers/tree-group.controller');
 const validation = require('../../validations/tree-group.validation');
 
+const Multer = require('multer');
+
 const router = express.Router();
 
-router.route('/').post(validate(validation.treeGroup), controller.createTreeGroup);
+const multer = Multer({
+  storage: Multer.memoryStorage(),
+  limits: {
+    fileSize: 5 * 1024 * 1024, // no larger than 5mb, you can change as needed.
+  },
+});
+
+router.route('/').post(multer.single('photo'), controller.createTreeGroup);
 
 module.exports = router;
